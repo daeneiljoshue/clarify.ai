d30bb80913c08fc451d4be0f449e7c8a15a50c9f
daeneiljoshue <159404797+daeneiljoshue@users.noreply.github.com>
2024-03-03 03:12:32 +0000

    check for problems and needs changes

---
+++ clarify/apps/dataset_manager/formats/vggface2.py
@@ -0,0 +1,30 @@
+
+import zipfile
+
+from datumaro.components.dataset import Dataset
+
+from clarify.apps.dataset_manager.bindings import GetCVATDataExtractor, TaskData, \
+    import_dm_annotations
+from clarify.apps.dataset_manager.util import make_zip_archive
+
+from .registry import dm_env, exporter, importer
+
+
+@exporter(name='VGGFace2', ext='ZIP', version='1.0')
+def _export(dst_file, temp_dir, instance_data, save_images=False):
+    with GetCVATDataExtractor(instance_data, include_images=save_images) as extractor:
+        dataset = Dataset.from_extractors(extractor, env=dm_env)
+        dataset.export(temp_dir, 'vgg_face2', save_images=save_images)
+
+    make_zip_archive(temp_dir, dst_file)
+
+@importer(name='VGGFace2', ext='ZIP', version='1.0')
+def _import(src_file, temp_dir, instance_data, load_data_callback=None, **kwargs):
+    zipfile.ZipFile(src_file).extractall(temp_dir)
+
+    dataset = Dataset.import_from(temp_dir, 'vgg_face2', env=dm_env)
+    if isinstance(instance_data, TaskData):
+        dataset.transform('rename', regex=r"|([^/]+/)?(.+)|\2|")
+    if load_data_callback is not None:
+        load_data_callback(dataset, instance_data)
+    import_dm_annotations(dataset, instance_data)
