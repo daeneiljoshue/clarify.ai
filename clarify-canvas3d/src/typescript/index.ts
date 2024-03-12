df797c16155a88e1300936b84e3c53a5d45bcf8a
daeneiljoshue <159404797+daeneiljoshue@users.noreply.github.com>
2024-03-12 03:45:30 +0000

    update these files

---
+++ clarify-canvas3d/src/typescript/index.ts
@@ -0,0 +1,9 @@
+
+
+import ObjectState from 'clarify-core/src/object-state';
+import { Label } from 'clarify-core/src/labels';
+import { ShapeType, ObjectType } from 'clarify-core/src/enums';
+
+export {
+    ObjectState, Label, ShapeType, ObjectType,
+};
