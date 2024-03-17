c3d8edef6bf2709b01f26aaea452585c72975e3d
daeneiljoshue <159404797+daeneiljoshue@users.noreply.github.com>
2024-03-17 03:30:38 +0000

    update these files

---
+++ tests/cypress/e2e/issues_prs/issue_5381_empty_project_formats.js
@@ -0,0 +1,29 @@
+
+
+/// <reference types="cypress" />
+
+const issueId = '5381';
+const project = {
+    name: `Issue ${issueId}`,
+    label: 'Tree',
+    attrName: 'Kind',
+    attrVaue: 'Oak',
+};
+
+context('List of export formats for a project without tasks is not empty', () => {
+    before(() => {
+        cy.visit('auth/login');
+        cy.login();
+        cy.goToProjectsList();
+        cy.createProjects(project.name, project.label, project.attrName, project.attrVaue);
+        cy.goToProjectsList();
+    });
+
+    describe(`Testing issue "${issueId}"`, () => {
+        it('Export formats list is not empty for empty project', () => {
+            cy.clickInProjectMenu('Export dataset', false, project.name);
+            cy.get('.cvat-modal-export-project').should('be.visible').find('.cvat-modal-export-select').click();
+            cy.get('.cvat-modal-export-option-item').should('have.length.above', 0);
+        });
+    });
+});
