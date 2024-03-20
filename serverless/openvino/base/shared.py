136d6b6db94e9401e3b16dc97f74cd545f5c0ac0
daeneiljoshue <159404797+daeneiljoshue@users.noreply.github.com>
2024-03-20 05:59:15 +0000

    check if these files need to be updated

---
+++ serverless/openvino/base/shared.py
@@ -0,0 +1,7 @@
+
+
+def to_clarify_mask(box: list, mask):
+    xtl, ytl, xbr, ybr = box
+    flattened = mask[ytl:ybr + 1, xtl:xbr + 1].flat[:].tolist()
+    flattened.extend([xtl, ytl, xbr, ybr])
+    return flattened
